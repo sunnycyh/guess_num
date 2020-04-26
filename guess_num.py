c6b3b61243e192ad7b654d4acbf8160e0f2351a5
Sunny Choi <yukhengchoi@gmail.com>
2020-04-26 18:04:58 +0800

    final version

--- guess_num.py
+++ guess_num.py
@@ -1,10 +1,15 @@
 import random
-r = random.randint(1,100)
+end = input('Please enter the biggest number: ')
+start = input('Please enter the smallest number: ')
+end = int(end)
+start = int(start)
+
+r = random.randint(start,end)
 r = int(r)
 count = 0
 while True:
 	count += 1 # count = count + 1
-	g = input('Please guess a number from 1 to 100: ')
+	g = input('Please guess a number: ')
 	g = int(g)
 	if g == r:
 		print('Yeah! You are correct!')
